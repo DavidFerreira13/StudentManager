3933b8e3b25d5ae7528a41139e664285cd78005f
David Ferreira <62451188+DavidFerreira13@users.noreply.github.com>
2022-05-05 14:45:16 +0100

    Add interface header

---
+++ interface.h
@@ -0,0 +1,16 @@
+#ifndef _INTERFACE_H
+#define _INTERFACE_H
+
+void menu_principal();
+void adicionar();
+void listar();
+void alterar();
+void apagar();
+void calcular();
+void gravar();
+void sair();
+
+int obter_escolha(int numero_de_escolhas);
+void limpar_ecra();
+
+#endif
